634f6181ba1e4e32d4e1e27a9700858db0a43500
jossyahia <jossyahia@gmail.com>
2023-07-26 04:37:03 +0100

    admin

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ import NavbarItem from "./NavbarItem";
 export default function Navbar() {
   return (
     <div className="flex justify-center dark:bg-gray-600 bg-amber-100 lg:text-lg p-4">
-      <NavbarItem title="Trending" param="fetchTrending" />
+      <NavbarItem title="Admin" param="admin" />
       <NavbarItem title="Rider" param="rider" />
       <NavbarItem title="Shop" param="shop" />
     </div>
